637c9f320c0762c6f815bd570e97ceb30a507fc6
haz15 <haz15@aber.ac.uk>
2023-09-11 16:22:43 +0800

    margin

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from fpdf import FPDF
 import os
 import base64
 import openai
-from flask import jsonify  # 导入jsonify函数
+from flask import jsonify  
 from flask import render_template
 from flask import Flask, request, send_file
 from werkzeug.utils import secure_filename
@@ -19,17 +19,16 @@ import tiktoken
 tokenizer = tiktoken.get_encoding("cl100k_base")
 
 
-# 忽略FPDF库的特定警告
+
 warnings.filterwarnings("ignore", category=UserWarning,
                         module="fpdf.ttfonts", lineno=670)
 
 
 app = Flask(__name__, static_folder='static')
-app.config['UPLOAD_FOLDER'] = 'uploads/'  # 设置上传文件的存储路径
+app.config['UPLOAD_FOLDER'] = 'uploads/'  
 
-cors = CORS(app, resources={r"/*": {"origins": "*"}})  # 允许所有域名访问
+cors = CORS(app, resources={r"/*": {"origins": "*"}})  
 
-# 替换为你的OpenAI API key
 openai.api_key = os.getenv('OPENAI_API_KEY')
 
 max_length = 13000
@@ -55,7 +54,6 @@ def upload_file():
             text = extract_text_from_pdf(file_path)
             texts.append(text)
 
-    # 返回一个包含多个文本的JSON对象
     return jsonify({'texts': texts})
 
 
@@ -91,7 +89,6 @@ def remove_duplicates_with_gpt(text):
 
     messages = [{"role": "system", "content": "Please remove any repetitive information from the text. Please note that only the repetitive information should be removed and nothing else should be changed."}]
     messages.append({"role": "user", "content": text})
-    # 定义提示，让模型知道它需要删除重复的内容
 
     response = openai.ChatCompletion.create(
         model="gpt-4",
@@ -104,35 +101,31 @@ def remove_duplicates_with_gpt(text):
 def summarize_with_gpt_longText(text, prompt):
     paragraphs = split_text_into_paragraphs(text)
 
-    # 初始化一个空的结果列表来保存GPT-3的所有答案
     results = []
 
-    # 将段落作为助理消息添加到messages列表中
     for paragraph in paragraphs:
 
-        # 创建一个messages列表并添加一个系统消息
         messages = [
             {"role": "system", "content": "You are an assistant who extract scientific data and summarise them for researchers."}]
-        # 将段落和问题添加到消息中
+
         messages.append({"role": "user", "content": paragraph})
         messages.append({"role": "assistant", "content": prompt})
 
-        # 创建聊天完成请求
+
         response = openai.ChatCompletion.create(
             model="gpt-3.5-turbo-16k",
             messages=messages
         )
 
-        # 保存GPT-3的答案
+
         answer = response.choices[0].message['content']
         results.append(answer)
 
         messages = []
 
-    # 最终，results包含了从每一段中提取的关键信息
+
     final_output = ' '.join(results)
 
-    # 使用 GPT 模型删除整个文本的重复内容
     final_output = remove_duplicates_with_gpt(final_output)
 
     return final_output
@@ -166,68 +159,6 @@ def generate_summary():
 
     return jsonify({'summary': summary})
 
-
-# @app.route('/downloadSummaries', methods=['POST'])
-# def download_summaries():
-#     # 从请求中获取摘要内容
-#     summaries_data_list = request.json['summaries']  # 获取整个摘要列表
-
-#     # 创建PDF文档
-#     pdf = FPDF()
-#     pdf.add_page()
-
-#     # 加载DejaVu字体
-#     pdf.add_font('DejaVu', '', 'DejaVuSansCondensed.ttf', uni=True)
-
-#     def split_text(text, max_width, font_size):
-#         words = text.split(' ')
-#         lines = []
-#         line = ''
-#         for word in words:
-#             if pdf.get_string_width(line + word) < max_width:
-#                 line += ' ' + word
-#             else:
-#                 lines.append(line)
-#                 line = word
-#         lines.append(line)
-#         return lines
-
-#     font_size = 11
-#     pdf.set_font('DejaVu', size=font_size)
-
-#     # 单元格宽度
-#     col_widths = [80, 110]
-#     max_width = col_widths[0] - 2  # 留出一点边距
-
-#     # 遍历所有摘要，并将它们添加到PDF文档中
-#     for summaries_data in summaries_data_list:
-#         for summary_group in summaries_data:
-#             title = summary_group['title']
-#             content = summary_group['content']
-
-#             # 将title拆分为多行
-#             title_lines = split_text(title, max_width, font_size)
-#             num_lines = len(title_lines)
-
-#             # 添加title和content
-#             for i, line in enumerate(title_lines):
-#                 pdf.cell(col_widths[0], 10, txt=line, border=1)
-#                 if i == 0:
-#                     pdf.multi_cell(
-#                         col_widths[1], 10 * num_lines, txt=content, border=1)
-#                 else:
-#                     pdf.cell(col_widths[1], 10, txt='', border=1)
-#                 pdf.ln()
-
-#     # 保存PDF到临时文件
-#     pdf_path = "temp_summaries.pdf"
-#     pdf.output(pdf_path)
-
-#     # 打开PDF文件
-#     f = open(pdf_path, 'rb')
-
-#     # 发送PDF文件作为响应
-#     return send_file(f, as_attachment=True, download_name='summaries.pdf')
 
 def send_email_with_attachment(to_email, subject, content, pdf_path):
     from_email = 'admin@ai.samastar.co.uk'
@@ -263,11 +194,11 @@ def send_email_with_attachment(to_email, subject, content, pdf_path):
 
 
 def create_pdf(pdf_path, summaries_data_list):
-    # 创建PDF文档
+
     pdf = FPDF()
     pdf.add_page()
 
-    # 加载DejaVu字体
+
     pdf.add_font('DejaVu', '', 'DejaVuSansCondensed.ttf', uni=True)
 
     def split_text(text, max_width, font_size):
@@ -286,21 +217,20 @@ def create_pdf(pdf_path, summaries_data_list):
     font_size = 11
     pdf.set_font('DejaVu', size=font_size)
 
-    # 单元格宽度
     col_widths = [80, 110]
-    max_width = col_widths[0] - 2  # 留出一点边距
+    max_width = col_widths[0] - 2  
 
-    # 遍历所有摘要，并将它们添加到PDF文档中
+    
     for summaries_data in summaries_data_list:
         for summary_group in summaries_data:
             title = summary_group['title']
             content = summary_group['content']
 
-            # 将title拆分为多行
+
             title_lines = split_text(title, max_width, font_size)
             num_lines = len(title_lines)
 
-            # 添加title和content
+
             for i, line in enumerate(title_lines):
                 pdf.cell(col_widths[0], 10, txt=line, border=1)
                 if i == 0:
@@ -310,30 +240,30 @@ def create_pdf(pdf_path, summaries_data_list):
                     pdf.cell(col_widths[1], 10, txt='', border=1)
                 pdf.ln()
 
-    # 保存PDF到临时文件
+
     pdf.output(pdf_path)
 
 
 @app.route('/sendEmail', methods=['POST'])
 def send_email():
-    # 从请求中获取数据
+
     data = request.json
     name = data['name']
     email = data['email']
     summaries_data_list = data['summaries']
 
-    # 创建 PDF 文件
+
     pdf_path = "temp_summaries.pdf"
     create_pdf(pdf_path, summaries_data_list)
 
-    # 发送电子邮件
+
     subject = "Your PDF Summary"
     content = "Hello " + name + ",\n\nHere is your PDF summary."
     status, _, _ = send_email_with_attachment(
         email, subject, content, pdf_path)
 
-    # 删除临时 PDF 文件
-    if status == 202:  # 只有在电子邮件发送成功时删除临时文件
+    
+    if status == 202:  
         os.remove(pdf_path)
 
     return jsonify({'status': 'ok'})
@@ -341,16 +271,16 @@ def send_email():
 
 @app.route('/sendFeedback', methods=['POST'])
 def send_feedback():
-    # 从请求中获取数据
+
     data = request.json
     rating = data['rating']
     feedback = data['feedback']
 
-    # 创建邮件内容
+
     subject = "New Feedback Received"
     content = f"Rating: {rating}\n\nFeedback:\n{feedback}"
 
-    # 发送电子邮件到指定邮箱
+    
     to_email = 'admin@ai.samastar.co.uk'
     _, _, _ = send_email_with_attachment(to_email, subject, content, None)
 
@@ -358,7 +288,7 @@ def send_feedback():
 
 
 if __name__ == '__main__':
-    if not os.path.exists(app.config['UPLOAD_FOLDER']):  # 如果存储路径不存在，创建一个新的
+    if not os.path.exists(app.config['UPLOAD_FOLDER']):  
         os.makedirs(app.config['UPLOAD_FOLDER'])
 
     app.run(debug=True)
